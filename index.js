fa582637f1dc9a66a94578f704d408966e7516b2
Li Ying <infinitesekai@gmail.com>
2022-11-30 17:35:36 +0800

    Niital

--- index.js
+++ index.js
@@ -2,8 +2,24 @@
  * @format
  */
 
-import {AppRegistry} from 'react-native';
+import { AppRegistry } from 'react-native';
 import App from './App';
-import {name as appName} from './app.json';
+import { name as appName } from './app.json';
+import { persistStore } from 'redux-persist';
+import * as React from 'react';
+import store from './App/Redux/store';
+import { Provider, } from 'react-redux';
+import { PersistGate } from 'redux-persist/integration/react';
 
-AppRegistry.registerComponent(appName, () => App);
+const persist = persistStore(store);
+
+
+const app = () => (
+  <Provider store={store}>
+    <PersistGate loading={null} persistor={persist}>
+      <App />
+    </PersistGate>
+  </Provider>
+);
+
+AppRegistry.registerComponent(appName, () => app);
